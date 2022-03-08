7f1b8778b759a3a64a9419247b31d34e6c57933b
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-03-08 19:43:44 -0300

    adicionando o typescript, estanciando uma aplicação com express e mapeando uma rota

--- aula-01/src/index.ts
+++ aula-01/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Executei')
+import express,  {Request, Response, NextFunction} from 'express';
+
+// O express é um gerenciador de rotas para gerenciar rotas http
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'bar'});
+});
+
+app.listen(3000, () => {
+    console.log("Aplicação executando na porta 3000")
+});
